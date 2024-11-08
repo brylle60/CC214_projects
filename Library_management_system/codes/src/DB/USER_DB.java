80728078f241a202d0c6a0137d1d087b2c47131b
brylle60 <johnbryllecrodua77@gmail.com>
2024-11-08 17:04:55 +0800

    db Initial commit

--- Library_management_system/codes/src/DB/USER_DB.java
+++ Library_management_system/codes/src/DB/USER_DB.java
@@ -27,5 +27,36 @@ public class USER_DB {
             //return false;
         }
 
+        public static boolean validate(int Id, String Name, String password){
+        try {
+            Connection connection = DriverManager.getConnection(DB_Connection.url, DB_Connection.user, DB_Connection.pass);
+            PreparedStatement valid = connection.prepareStatement("SELECT * FROM "+DB_Connection.tab+" WHERE Id = ? AND password = ?");
 
+            valid.setInt(1, Id);
+            valid.setString(2, Name);
+            valid.setString(3, password);
+
+            valid.executeQuery();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static boolean checkuser(int Id){
+        try {
+            Connection connection = DriverManager.getConnection(DB_Connection.url, DB_Connection.user, DB_Connection.pass);
+            PreparedStatement check = connection.prepareStatement("SELECT * FROM "+DB_Connection.tab+" WHERE Id = ?");
+
+            check.setInt(1, Id);
+
+            check.executeQuery();
+            return true;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
