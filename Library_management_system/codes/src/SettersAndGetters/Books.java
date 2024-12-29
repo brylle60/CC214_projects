ee88514cf619f7edbbc0c11a6c235764d37b6865
P-Chel <chelsiefaith07@gmail.com>
2024-12-29 15:29:10 +0800

    return class and setters for books object

--- Library_management_system/codes/src/SettersAndGetters/Books.java
+++ Library_management_system/codes/src/SettersAndGetters/Books.java
@@ -20,9 +20,6 @@ public class Books {
     private static int totalCopy;
     private String currentBorrower;
     private LocalDateTime borrowDate;
-    private boolean borrowed;
-    private String borrower;
-    private boolean isAvailable;
 
     public Books(int ISBN, String title, String author, LocalDateTime datePublished, int availableCopy, int totalCopy) {
         this.ISBN = ISBN;
@@ -36,6 +33,37 @@ public class Books {
         this.borrowed = false;
         this.borrower = "";
     }
+public Books(){};
+
+    public static void setISBN(int ISBN) {
+        Books.ISBN = ISBN;
+    }
+
+    public static void setTitle(String title) {
+        Books.title = title;
+    }
+
+    public static void setAuthor(String author) {
+        Books.author = author;
+    }
+
+    public static void setDatePublished(LocalDateTime datePublished) {
+        Books.datePublished = datePublished;
+    }
+
+    public static void setTotalCopy(int totalCopy) {
+        Books.totalCopy = totalCopy;
+    }
+
+    public void setCurrentBorrower(String currentBorrower) {
+        this.currentBorrower = currentBorrower;
+    }
+
+
+    private boolean borrowed;
+    private String borrower;
+    private boolean isAvailable;
+
 
     // Getters and setters
     public int getISBN() {
